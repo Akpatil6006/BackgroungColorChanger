b4dd96aab70ba7e4b6652753b020d7ebdef02582
Ajay Katare <91534499+Akpatil6006@users.noreply.github.com>
2023-12-06 12:43:25 +0530

    Add files via upload

---
+++ 1.Backgroung Color Changer/script.js
@@ -0,0 +1,12 @@
+const greyColor = function() {
+    document.body.style.background = "grey";
+}
+const blueColor = function() {
+    document.body.style.background = "blue";
+}
+const yellowColor = function() {
+    document.body.style.background = "yellow";
+}
+const orangeColor = function() {
+    document.body.style.background = "orange";
+}
